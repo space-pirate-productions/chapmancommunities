652f7a6213291d80efc08192683e99da54a1e337
Stephen Parker <stephen.parker@gmail.com>
2022-08-10 16:36:34 -0400

    fine tune team directory slider

--- resources/scripts/components/team-directory-slider.js
+++ resources/scripts/components/team-directory-slider.js
@@ -3,17 +3,21 @@ import $ from 'jquery';
 
 const teamsSlider = {
   init() {
-    let locationCount = $('#cc-team-locations').find('.cc-team-directory__location').length;
     let navigationSettings = {
       isNavigation: true,
       autoWidth: true,
       arrows: false,
       pagination: false,
+      drag: false,
+      updateOnMove: true,
+      snap: true,
+      focus: 'center',
     };
     let directorySettings = {
       type: 'fade',
       arrows: false,
       pagination: false,
+      drag: false,
     };
 
     let navigation = new Splide('#cc-team-locations', navigationSettings);
@@ -22,6 +26,36 @@ const teamsSlider = {
     navigation.sync(directory);
     navigation.mount();
     directory.mount();
+
+    function adjustSlides() {
+      let totalSlideWidth = null;
+      let slideContainerWidth = $('#cc-team-locations-list').innerWidth();
+      let slides = $('#cc-team-locations.is-active').find('.cc-team-directory__location');
+      slides.each(function() {
+        let slideWidth = $(this).outerWidth(false);
+        totalSlideWidth += slideWidth;
+      });
+
+      if (Math.floor(totalSlideWidth) > Math.floor(slideContainerWidth)) {
+        if (navigation.options.arrows == false) {
+          navigation.options = {
+            arrows: true,
+            drag: 'free',
+          }
+        }
+      } else {
+        if (navigation.options.arrows == true) {
+          navigation.options = {
+            arrows: false,
+            drag: false,
+          }
+        }
+      }
+      }
+
+      $(window).on('load resize', function(){
+        adjustSlides();
+      })
   }
 };
 
