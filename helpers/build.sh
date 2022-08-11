50c9acdc26fc3e33d20f9b4a04eaed84c1ea250f
Stephen Parker <stephen.parker@gmail.com>
2022-08-11 11:06:51 -0400

    remove git files from release package

--- helpers/build.sh
+++ helpers/build.sh
@@ -1,4 +1,4 @@
 cd ..
-zip -r chapmancommunities/chapmancommunities.zip chapmancommunities -x chapmancommunities/.github/\* chapmancommunities/node_modules/\*
+zip -r chapmancommunities/chapmancommunities.zip chapmancommunities -x chapmancommunities/.github/\* chapmancommunities/node_modules/\* chapmancommunities/.git/\* chapmancommunities/.gitignore
 echo $PWD
 ls
