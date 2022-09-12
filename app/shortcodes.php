8d04f7424fcb485e1c29907242376b3ad5935b83
Stephen Parker <stephen.parker@gmail.com>
2022-09-12 13:56:55 -0400

    fix: add button shortcode

--- app/shortcodes.php
+++ app/shortcodes.php
@@ -6,4 +6,20 @@ function cc_copyright_shortcode(){
     return $output;
 }
 add_shortcode( 'cc-copyright', __NAMESPACE__ . '\\cc_copyright_shortcode' );
+
+function cc_button_shortcode($atts) {
+    $a = shortcode_atts(array(
+        "element" => "anchor",
+        "type" => "",
+        "href" => "",
+        "text" => "",
+    ), $atts);
+
+    if ($a['element'] == "anchor") {
+        return "<a class='btn cc-button' href='" . $a['href'] . "'>" . $a['text'] . "</a>";
+    } else {
+        return "<button class='btn cc-button' type='" . $a['type'] . "'>" . $a['text'] . "</button>";
+    }
+}
+add_shortcode('cc-button',__NAMESPACE__ . '\\cc_button_shortcode' );
 ?>
