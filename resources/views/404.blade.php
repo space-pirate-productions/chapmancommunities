e8d37c4730f88c3300cf554cd71f41fd09565e75
Stephen Parker <stephen.parker@gmail.com>
2022-09-12 13:21:12 -0400

    fix:  add 404 page
    add and style 404 page
    
    ✅ Closes: #3

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
 
-  @if (! have_posts())
-    <x-alert type="warning">
-      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-    </x-alert>
+@if (! have_posts())
+<div class="chapman-404-message text-center">
+  <h1 class="chapman-404-message__title">{!! __('404', 'sage' ) !!}</h1>
+  <h2 class="chapman-404-message__sub-title">{!! __('Page Not Found','sage' ) !!}</h2>
+  <x-alert type="warning">
+    {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
+  </x-alert>
 
-    {!! get_search_form(false) !!}
-  @endif
+  {!! get_search_form(false) !!}
+</div>
+@endif
 @endsection
