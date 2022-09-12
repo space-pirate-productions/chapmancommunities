763991255399e193df78356cb4490a2647751c1a
Stephen Parker <stephen.parker@gmail.com>
2022-09-12 15:15:30 -0400

    fix: blog hero
    replace `$post_id` with `$post->ID` in hero section

--- resources/views/sections/hero.blade.php
+++ resources/views/sections/hero.blade.php
@@ -1,5 +1,5 @@
-@if (get_the_post_thumbnail($post_id, 'featured-image') && !(is_archive() || is_home()))
+@if (get_the_post_thumbnail($post->ID, 'featured-image') && !(is_archive() || is_home()))
 <section class="cc-hero container d-flex justify-content-center">
-  {!! get_the_post_thumbnail($post_id, 'featured-image', ['class' => 'img-fluid']) !!}
+  {!! get_the_post_thumbnail($post->ID, 'featured-image', ['class' => 'img-fluid']) !!}
 </section>
 @endif
