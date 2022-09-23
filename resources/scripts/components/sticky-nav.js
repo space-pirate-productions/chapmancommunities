044d24d032a193551173369532d8e89f9483608d
Space Pirate Productions <110199482+space-pirate-productions@users.noreply.github.com>
2022-09-22 20:11:31 -0400

    fix: sticky nav (#33)
    closes: #30
    
    Co-authored-by: Stephen Parker <stephen.parker@gmail.com>

---
+++ resources/scripts/components/sticky-nav.js
@@ -0,0 +1,14 @@
+const stickyNav = {
+  init() {
+    const el = document.querySelector("#main > .document >:first-child");
+    const banner = document.querySelector("#cc-banner");
+    const observer = new IntersectionObserver(
+      ([e]) => banner.classList.toggle("cc-banner--stuck", e.intersectionRatio < 1),
+      { threshold: [1] }
+    );
+
+    observer.observe(el);
+  }
+};
+
+export default stickyNav;
--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,4 +1,4 @@
-<header class="cc-banner">
+<header id="cc-banner" class="cc-banner">
   <div class="cc-banner__container navbar navbar-expand-lg">
     <div class="cc-banner__logo">
       <a class="navbar-brand" href="{{ home_url('/') }}">
@@ -16,25 +16,25 @@
     </button>
     <div class="cc-banner__nav collapse navbar-collapse justify-content-lg-end" id="cc-primary-nav">
       <div class="cc-banner__nav-wrap">
-      @if (has_nav_menu('primary_navigation'))
-      <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-        {!! wp_nav_menu([
-        'theme_location' => 'primary_navigation',
-        'container' => false,
-        'menu_class' => '',
-        'fallback_cb' => '__return_false',
-        'items_wrap' => '<ul id="%1$s" class="navbar-nav %2$s">%3$s</ul>',
-        'depth' => 3,
-        'walker' => new App\bootstrap_5_wp_nav_menu_walker(),
-        ]) !!}
-      </nav>
-      @endif
-      @if (get_theme_mod('nav_button_text') && get_theme_mod('nav_button_url'))
-      <div class="cc-banner__cta">
-        <a href="{!! get_theme_mod('nav_button_url') !!}" class="btn">{{ get_theme_mod('nav_button_text') }}</a>
+        @if (has_nav_menu('primary_navigation'))
+        <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+          {!! wp_nav_menu([
+          'theme_location' => 'primary_navigation',
+          'container' => false,
+          'menu_class' => '',
+          'fallback_cb' => '__return_false',
+          'items_wrap' => '<ul id="%1$s" class="navbar-nav %2$s">%3$s</ul>',
+          'depth' => 3,
+          'walker' => new App\bootstrap_5_wp_nav_menu_walker(),
+          ]) !!}
+        </nav>
+        @endif
+        @if (get_theme_mod('nav_button_text') && get_theme_mod('nav_button_url'))
+        <div class="cc-banner__cta">
+          <a href="{!! get_theme_mod('nav_button_url') !!}" class="btn">{{ get_theme_mod('nav_button_text') }}</a>
+        </div>
+        @endif
       </div>
-      @endif
-    </div>
     </div>
     @endif
 
