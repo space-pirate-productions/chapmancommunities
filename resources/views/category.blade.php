a416834ebc689f0d92d23e1682369f86cea9e025
Stephen Parker <stephen.parker@gmail.com>
2022-10-03 17:01:40 -0400

    fix: category index formatting

---
+++ resources/views/category.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+@include('partials.page-header')
+
+@if (! have_posts())
+<x-alert type="warning">
+  {!! __('Sorry, no results were found.', 'sage') !!}
+</x-alert>
+
+{!! get_search_form(false) !!}
+@endif
+
+<div class="cc-post-index-archive cc-post-index-archive--category">
+  @while(have_posts()) @php(the_post())
+  @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+  @endwhile
+</div>
+
+{!! get_the_posts_navigation() !!}
+@endsection
+
+@section('sidebar')
+@include('sections.sidebar')
+@endsection
