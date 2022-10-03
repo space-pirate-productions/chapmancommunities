6e29990fa97d67be50a994f76c7320eb5aba1d97
Stephen Parker <stephen.parker@gmail.com>
2022-10-03 16:43:41 -0400

    fix: get correct post id/thumbnail
    On the blog index get the correct ID and featured image for each post.

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,9 +1,9 @@
-<article @php(post_class(array( 'entry', 'd-flex', 'justify-content-center' )))>
+<article @php(post_class(array( 'entry' , 'd-flex' , 'justify-content-center' )))>
 
   <div class="entry-wrap">
     <div class="entry-image">
       <a href="{{ get_permalink() }}">
-      {!! get_the_post_thumbnail( $post->ID, 'blog-index' ) !!}
+        {!! get_the_post_thumbnail( get_the_ID(), 'blog-index' ) !!}
       </a>
     </div>
 
