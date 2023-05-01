795410a1e4204bfb6e08faec4513b98bdfa42e7b
Space Pirate Productions <parker.stephend@gmail.com>
2023-05-01 19:29:28 -0400

    fix: remove page/post hero images and reduce font size for homepage hero

--- resources/views/sections/widehero.blade.php
+++ resources/views/sections/widehero.blade.php
@@ -1,5 +1,5 @@
-@if (get_the_post_thumbnail(get_the_ID(), 'homepage-hero'))
+{{-- @if (get_the_post_thumbnail(get_the_ID(), 'homepage-hero'))
 <section class="cc-hero cc-container cc-container--wide d-flex justify-content-center">
   {!! get_the_post_thumbnail(get_the_ID(), 'homepage-hero', ['class' => 'img-fluid']) !!}
 </section>
-@endif
+@endif --}}
